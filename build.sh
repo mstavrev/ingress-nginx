428ec3e3858af11d8602436429c0683777cbd876
Marin Stavrev <mstavrev@gmail.com>
2020-04-28 09:35:30 +0300

    Update build.sh script to automatically build nginx prior to nginx-ingress docker image

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -e
+
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-make build && make container && make push
+# Build the nginx (web server) image itslef first:
+cd images/nginx
+make container && make push
 
+# Build the nginx-ingress image:
+cd ../..
+make build && make container && make push
